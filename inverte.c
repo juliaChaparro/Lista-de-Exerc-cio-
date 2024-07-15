9575fc25e2f6b45641c653ea6788d8cc14b149d6
Julia Chaparro <157522106+juliaChaparro@users.noreply.github.com>
2024-07-14 21:20:11 -0400

    Inverte um vetor

---
+++ inverte.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+
+int main(){
+    int vet [10];
+    int i;
+    int x;
+    int cont = 0;
+
+    for (i = 0 ;i<8;i++){
+        scanf("%d",&x);
+        vet[i]= x;
+        cont ++;
+    }
+    while(cont!=0){
+        printf("%d\n",vet[cont-1]);
+        cont --;
+    }
+}
