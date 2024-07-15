5be21e2e5d0db52966ae36e2f7f86d5df89cee45
Julia Chaparro <157522106+juliaChaparro@users.noreply.github.com>
2024-07-15 00:06:33 -0400

    inverteV1.c

--- inverteV1.c
+++ inverteV1.c
@@ -1,24 +1,25 @@
 #include<stdio.h>
 
-
 int main(){
-    int vet [8];
+    int vet [8]={1,2,,3,4,5,6,7,8};
     int i=0,j=8;
     int x;
     int cont = 0;
-    int naosei;
-    int aux;
-    for (i = 0 ;i<8;i++){
-        scanf("%d",&x);
-        vet[i]= x;
-        cont ++;
-    }
+    int aux = i ;
 
-    while (i<j){
-        vet[i]=vet[aux];
-        vet[i]= vet[j];
-        vet [j]=vet[aux]; 
+
+    while (i < j){
+        vet[aux] = vet[i];
+        vet[i]=vet[j];
+        vet[j]= vet[aux];
+
+        aux ++;
         i++;
         j--;
     }
+
+    for(i=0;i<8;i++){
+        printf("%d\n",vet[i]);
+    }
+
 }
