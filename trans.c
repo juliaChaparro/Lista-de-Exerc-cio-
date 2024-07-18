76a97b64c27e867e35d3a29bb2625ff7bdc4008f
juliaChaparro <julia.ferreira@icomp.ufam.edu.br>
2024-07-17 20:44:20 -0400

    matriz

--- trans.c
+++ trans.c
@@ -2,9 +2,11 @@
 
 int main(){
     int mata [3][3];
+    int matb [3][3];
     int i;
     int j;
     int x;
+    int c =0;
 
     for (i=0;i<3;i++){
         for (j=0;j<3;j++){
@@ -13,10 +15,16 @@ int main(){
         }
     }
 
+    for (int l=0;l<3;l++){
+        for (int c=0;c<3;c++){
+            matb [l][c]= mata[c][l];
+        }
+    }
+
     for(int l=0;l<3;l++){
         for (int c=0;c<3;c++){
-                printf("%d ",mata[l][c]);
+                printf("%d ",matb[l][c]);
         }
-        printf("%d\n");
+        printf(" \n");
     }
 }
