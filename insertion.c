e286e1b6b3f5600314ae4c3eca4576b6fad16f22
juliaChaparro <julia.ferreira@icomp.ufam.edu.br>
2024-07-29 20:27:16 -0400

    insertionSort

---
+++ insertion.c
@@ -0,0 +1,35 @@
+
+#include<stdio.h>
+
+void insertion(int vet[],int tam){
+    int i, j, pivot;
+
+    for (i = 1;i<tam;i++){
+        pivot = vet [i];
+        j = i-1;
+        while((j >= 0) && (vet[j]>pivot)){
+            vet[j+1]=vet[j];
+            j--;
+        }
+        vet [j+1]= pivot;
+    }
+}
+
+
+int main(){
+    int tam = 5;
+    int i = 0,j=0 ,vet[5];
+    int x = 0;
+
+    while (i != 5){
+        scanf("%d",&x);
+        vet[i]= x;
+        i++;
+    }
+
+    insertion(vet,5);  
+
+    for(j=0;j<5;j++){
+        printf("%d\n",vet[j]);
+    }
+}
